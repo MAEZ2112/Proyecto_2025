6272bca416f6478f95bb94e59094bd8ee18942a5
Vanessa Valladares <100142668+vivq22@users.noreply.github.com>
2025-04-08 00:02:38 -0600

    Add requisitos page and related components; remove nosotros page

--- data/index.js
+++ data/index.js
@@ -1,5 +1,7 @@
 import { indexPage } from "./pages/indexPage";
 import { serviciosPage } from "./pages/serviciosPage.js"
+import { requisitosPage } from "./pages/requisitosPage.js";
+
 
 const commonData = {
     navigation: [
@@ -22,6 +24,10 @@ export const getPageContext = (pagePath) => {
 
         case '/main/servicios/servicios.html':
             pageData = serviciosPage
+            break;
+        
+        case '/main/recursos_legales/requisitos.html':
+            pageData = requisitosPage
             break;
             
         default:
