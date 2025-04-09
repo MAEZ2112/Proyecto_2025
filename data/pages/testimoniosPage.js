bb099c91c991a8b215c5df7f7bee8bf53bccdcf6
Vanessa Valladares <100142668+vivq22@users.noreply.github.com>
2025-04-09 13:45:49 -0600

    Add new sections and components for requisitos, servicios, and testimonios pages; enhance HTML structure and Handlebars partials for better content management.

--- data/pages/testimoniosPage.js
+++ data/pages/testimoniosPage.js
@@ -18,5 +18,28 @@ export const testimoniosPage = {
             tituloCaso: "Defensa de Garantías Constitucionales",
             descripcionCaso: "En temas de interés nacional, ponemos nuestros conocimientos a disposición para enmendar leyes que puedan violar los derechos constitucionales de los ciudadanos, promoviendo la justicia y el respeto por la ley.",
         }
+    ],
+
+    testimonios_casos: [
+        {
+            nombre: "Juan Pérez",
+            trabajo: "Empresario",
+            comentario: "El equipo del bufete notarial me ayudó con la constitución de mi empresa de manera ágil y eficiente. Siempre estuvieron atentos a cada detalle, resolviendo todas mis dudas. Los recomiendo ampliamente por su profesionalismo y compromiso.",
+            rate: "★★★★★"
+        },
+
+        {
+            nombre: "María López",
+            trabajo: "Profesora Universitaria",
+            comentario: "Me asesoraron con la redacción de mi testamento, un trámite que había postergado por años. Me dieron tranquilidad al explicar cada paso con claridad y paciencia. Estoy muy agradecida por su atención personalizada.",
+            rate: "★★★★★"
+        },
+
+        {
+            nombre: "Carlos Gómez",
+            trabajo: "Abogado",
+            comentario: "Cuando necesité gestionar una escritura pública para una compra importante, este bufete fue mi mejor elección. Su equipo siempre estuvo disponible y el proceso fue transparente de principio a fin. ¡Un servicio de excelencia!",
+            rate: "★★★★★"
+        }
     ]
 }
